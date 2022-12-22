7395944bde10743783aa6f7bfbe2ccae5dd2e02a
Islam abu el-soud <100817976+Islam-abuelsoud@users.noreply.github.com>
2022-12-22 23:29:57 +0200

    day 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductItemComponent,
+    ProductItemDetailComponent,
+    ProductListComponent,
+    CartComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
