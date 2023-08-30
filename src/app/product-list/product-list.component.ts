88543736fed1d81e7b95a1a7c829d9c18ac5107a
Islam abu el-soud <100817976+Islam-abuelsoud@users.noreply.github.com>
2023-08-30 13:59:02 +0300

    .

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
 
-  constructor() { }
+export class ProductListComponent implements OnInit {
+  data: any[] = [];
+  constructor(private _DataService: DataService) { }
 
   ngOnInit(): void {
+    this.data = this._DataService.list;
+    console.log(this.data)
   }
 
+
+
+
+
 }
+
